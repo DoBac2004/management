c9e7ccedae040ed6b246b025191b215dfd2c6ce9
DoBac2004 <111523003+DoBac2004@users.noreply.github.com>
2024-01-29 16:44:17 +0700

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Location { get; set; }
+        public int NumberOfEmployees { get; set; }
+
+        // Thuộc tính điều hướng
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Rank { get; set; }
+
+        // Khóa ngoại
+        public int DepartmentId { get; set; }
+
+        // Thuộc tính điều hướng
+        public Department Department { get; set; }
+    }
+}
